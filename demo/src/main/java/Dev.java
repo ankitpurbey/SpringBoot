9be98d00076c136737e9f9d272513fdd96fe41c1
Ankit Kumar <ankitkumar847401@gmail.com>
2025-01-29 10:32:15 +0530

    .

---
+++ demo/src/main/java/Dev.java
@@ -0,0 +1,8 @@
+
+public class Dev {
+
+    public  void build(){
+        System.out.println("Hello this is ankit");
+    }
+
+}
--- demo/src/test/java/com/telusko/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/telusko/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.telusko.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplicationTests.class, args){
+		Dev obj = new Dev();
+		obj.build();
+	}
+}
 
 	@Test
 	void contextLoads() {
 	}
 
 }
+
